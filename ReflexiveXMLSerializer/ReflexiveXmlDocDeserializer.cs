6d442b43a985c714f405f74e3543c8b1ef102db0
LEVEQUE Franck <leveque.franck@gmail.com>
2016-05-28 23:15:16 +0200

    Added XmlDocument deserializer for compilation without Linq

---
+++ ReflexiveXMLSerializer/ReflexiveXmlDocDeserializer.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReflexiveXMLSerializer
+{
+    public class ReflexiveXmlDocDeserializer
+    {
+        private Dictionary<Type, List<Tuple<PropertyInfo, bool, string, Type>>> references;
+        private Type root;
+        public ReflexiveXmlDocDeserializer(Type objectType)
+        {
+            references = new Dictionary<Type, List<Tuple<PropertyInfo, bool, string, Type>>>();
+            root = objectType;
+            AddTypeToReferences(root);
+        }
+
+        private void AddTypeToReferences(Type refToAdd)
+        {
+            if (!references.ContainsKey(refToAdd))
+            {
+                references.Add(refToAdd, new List<Tuple<PropertyInfo, bool, string, Type>>());
+                foreach (PropertyInfo curProp in refToAdd.GetProperties())
+                {
+                    if (!Utility.IsBaseType(curProp.PropertyType))
+                    {
+                        if (curProp.PropertyType.GetInterfaces().Contains(typeof(System.Collections.IEnumerable)))
+                        {
+                            // We have a collection
+                            var typeToCheck = curProp.PropertyType.GetElementType();
+                            if (typeToCheck == null)
+                            {
+                                // We are in a List
+                                typeToCheck = curProp.PropertyType.GenericTypeArguments[0];
+                            }
+
+                            if (!Utility.IsBaseType(typeToCheck))
+                            {
+                                AddTypeToReferences(typeToCheck);
+                            }
+                        }
+                        else
+                        {
+                            AddTypeToReferences(curProp.PropertyType);
+                        }
+                    }
+
+                    references[refToAdd].Add(Tuple.Create(curProp, false, Utility.GetElementName(curProp).Name.ToString(), curProp.PropertyType));
+                }
+            }
+        }
+
+        public object Deserialize(string xml)
+        {
+            XmlDocument rootDoc = new XmlDocument();
+            rootDoc.LoadXml(xml);
+            XmlElement elem = rootDoc.DocumentElement;
+            
+            // We are in root object corresponding to the new type
+            return DeserializeObject(root, elem);
+        }
+
+        private object DeserializeObject(Type objectType,XmlElement elem)
+        {
+            object result = Activator.CreateInstance(objectType);
+            if (Utility.IsBaseType(objectType))
+            {
+                result = Convert.ChangeType(elem.Value, objectType);
+            }
+            else
+            {
+                foreach (var c in references[objectType])
+                {
+                    // Serialize elements
+                    if (c.Item2)
+                    {
+                        // Attribute property, normally a base type
+                        c.Item1.SetValue(result, Convert.ChangeType(elem.Attributes[c.Item3], c.Item1.PropertyType));
+                    }
+                    else
+                    {
+                        Type propType = c.Item4;
+                        if (!Utility.IsBaseType(propType) && propType.GetInterfaces().Contains(typeof(System.Collections.IEnumerable)))
+                        {
+                            // Instanciate a list of these elements
+                            Type elementType = propType.GetElementType();
+                            if (elementType == null)
+                            { 
+                                // We are in a List
+                                elementType = propType.GetGenericArguments()[0];
+                            }
+
+                            List<object> buffer = new List<object>();
+                            foreach (var d in elem.GetElementsByTagName(c.Item3).Item(0).ChildNodes)
+                            {
+                                if (Utility.IsBaseType(elementType))
+                                {
+                                    buffer.Add(Convert.ChangeType(((XmlElement)d).InnerText, elementType));
+                                }
+                                else
+                                {
+                                    buffer.Add(this.DeserializeObject(elementType, (XmlElement)d));
+                                }
+                            }
+
+
+                            MethodInfo castMethod = typeof(Enumerable).GetMethod("Cast")
+                                .MakeGenericMethod(new System.Type[] { elementType });
+                            MethodInfo toArrayMethod = null;
+                            if (propType.IsArray)
+                            {
+                                toArrayMethod = typeof(Enumerable).GetMethod("ToArray")
+                                .MakeGenericMethod(new System.Type[] { elementType });
+                            }
+                            else
+                            {
+                                toArrayMethod = typeof(Enumerable).GetMethod("ToList")
+                                    .MakeGenericMethod(new System.Type[] { elementType });
+                            }
+                            var castedObjectEnum = castMethod.Invoke(null, new object[] { buffer });
+                            c.Item1.SetValue(result, toArrayMethod.Invoke(null, new object[] { castedObjectEnum }));
+
+                        }
+                        else
+                        {
+                            var curElem = elem.GetElementsByTagName(c.Item3).Item(0);
+                            if (propType.IsEnum)
+                            {
+                                c.Item1.SetValue(result, Enum.Parse(c.Item1.PropertyType,curElem.InnerText));
+                            }
+                            else if (propType.IsGenericType && propType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+                            {
+                                if (!string.IsNullOrEmpty(curElem.InnerText))
+                                {
+                                    c.Item1.SetValue(result, Convert.ChangeType(curElem.InnerText, Nullable.GetUnderlyingType(propType), System.Globalization.CultureInfo.InvariantCulture));
+                                }
+                            }
+                            else
+                            {
+                                c.Item1.SetValue(result, Convert.ChangeType(curElem.InnerText, propType, System.Globalization.CultureInfo.InvariantCulture));
+                            }
+                        }
+                    }
+
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ReflexiveXmlSerializerUnitTests/DeserializationXmlDoc.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ForInLineBot;
+using NeuralNet.Serialization;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ReflexiveXmlSerializerUnitTests
+{
+    /// <summary>
+    /// Summary description for Deserialization
+    /// </summary>
+    [TestClass]
+    public class DeserializationXmlDoc
+    {
+        public DeserializationXmlDoc()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void SimpleObjectTest()
+        {
+            string xml = @"<SimpleTest><Id>1</Id><Name>Franck</Name><BirthDate>1978-9-16</BirthDate></SimpleTest>";
+            ReflexiveXMLSerializer.ReflexiveXmlDocDeserializer des = new ReflexiveXMLSerializer.ReflexiveXmlDocDeserializer(typeof(SimpleSample));
+            var t = des.Deserialize(xml);
+            Assert.IsInstanceOfType(t, typeof(SimpleSample));
+            var test = (SimpleSample)t;
+            Assert.AreEqual(1, test.Id);
+            Assert.AreEqual("Franck", test.Name);
+            Assert.AreEqual(new DateTime(1978,9,16), test.BirthDate);
+        }
+
+        [TestMethod]
+        public void ObjectArrayTest()
+        {
+            string xml = @"<ObjectArrayTest><Id>1</Id><Friends><SimpleTest><Id>1</Id><Name>Franck</Name><BirthDate>1978-9-16</BirthDate></SimpleTest></Friends></ObjectArrayTest>";
+            ReflexiveXMLSerializer.ReflexiveXmlDocDeserializer des = new ReflexiveXMLSerializer.ReflexiveXmlDocDeserializer(typeof(ObjectArraySample));
+            var t = des.Deserialize(xml);
+            Assert.IsInstanceOfType(t, typeof(ObjectArraySample));
+            var test = (ObjectArraySample)t;
+            Assert.AreEqual(1, test.Id);
+            //CollectionAssert.AreEqual(new string[] { "varaxor@free.fr", "leveque.franck@gmail.com", "franck.leveque2@free.fr" }, test.Mails);
+        }
+
+        [TestMethod]
+        public void SimpleArrayTest()
+        {
+            string xml = @"<SimpleTest><Id>1</Id><Mails><String>varaxor@free.fr</String><String>leveque.franck@gmail.com</String><String>franck.leveque2@free.fr</String></Mails></SimpleTest>";
+            ReflexiveXMLSerializer.ReflexiveXmlDocDeserializer des = new ReflexiveXMLSerializer.ReflexiveXmlDocDeserializer(typeof(SimpleArraySample));
+            var t = des.Deserialize(xml);
+            Assert.IsInstanceOfType(t, typeof(SimpleArraySample));
+            var test = (SimpleArraySample)t;
+            Assert.AreEqual(1, test.Id);
+            CollectionAssert.AreEqual(new string[] { "varaxor@free.fr", "leveque.franck@gmail.com", "franck.leveque2@free.fr" }, test.Mails);
+        }
+
+        [TestMethod]
+        public void NeuralTest()
+        {
+            string xml = NeuralData.Data;
+            ReflexiveXMLSerializer.ReflexiveXmlDocDeserializer des = new ReflexiveXMLSerializer.ReflexiveXmlDocDeserializer(typeof(Network));
+            var t = des.Deserialize(xml);
+            XmlSerializer xs = new XmlSerializer(typeof(Network));
+            Network b = (Network)xs.Deserialize(new MemoryStream(Encoding.ASCII.GetBytes(xml)));
+            Assert.AreEqual(b, t);
+        }
+    }
+}
